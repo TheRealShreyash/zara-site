21f17c4694a6c90c7dba3f87ed07ec5e4aab9aac
TheRealShreyash <koshta.shreyash02@gmail.com>
2024-12-02 17:12:40 +0530

    fixed scripts

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ const inviteButton = document.querySelector(".invitebutton");
 const supportButton = document.querySelector(".supportbutton");
 
 commandButton.addEventListener("click", function () {
-  window.location.href = "/commands.html";
+  window.location.href = "/zara-site/commands.html";
 });
 
 policyButton.addEventListener("click", function () {
-  window.location.href = "/policy.html";
+  window.location.href = "/zara-site/policy.html";
 });
 
 homeButton.addEventListener("click", function () {
-  window.location.href = "/index.html";
+  window.location.href = "/zara-site/index.html";
 });
 
 inviteButton.addEventListener("click", function () {
